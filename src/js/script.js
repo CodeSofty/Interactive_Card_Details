1d586baef456fc0cdf3412015a7f0cdcf27a8e9e
CodeSoftly <eclittlejohn@yahoo.com>
2022-08-25 22:28:45 -0500

    added JS file and class changes in html

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+
+
+// Form Input
+const cc_name_input = document.getElementsByClassName('cardholder_name_input');
+const cc_number_input = document.getElementsByClassName('card_number_input');
+const cc_exp_month_input = document.getElementsByClassName('card_exp_month_input');
+const cc_exp_year_input = document.getElementsByClassName('card_exp_year_input');
+const cc_cvc_input = document.getElementsByClassName('card_cvc_input');
+
+
+// DOM Elements
+const cc_name_el = document.getElementsByClassName('cardholder_name');
+const cc_number_el = document.getElementsByClassName('card_number');
+const cc_exp_date_el = document.getElementsByClassName('card_exp_date');
+const cc_cvc_el = document.getElementsByClassName('card_cvc');
+
+
+// On Key Up CVC Number
+
+
