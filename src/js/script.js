a9178138756482fccca3e0107b0864760b38aa4b
CodeSoftly <eclittlejohn@yahoo.com>
2022-08-27 22:40:00 -0500

    commit

--- src/js/script.js
+++ src/js/script.js
@@ -1,25 +1,23 @@
 
 
 // Form Input
-const cc_name_input = document.querySelector('#cardholder_name_input').textContent;
-const cc_number_input = document.querySelector('#card_number_input').textContent;
-const cc_exp_month_input = document.querySelector('#card_exp_month_input').textContent;
-const cc_exp_year_input = document.querySelector('#card_exp_year_input').textContent;
-const cc_cvc_input = document.querySelector('#card_cvc_input').textContent;
+
+let cc_number_input = document.querySelector('#card_number_input').value;
+let cc_exp_month_input = document.querySelector('#card_exp_month_input').value;
+let cc_exp_year_input = document.querySelector('#card_exp_year_input').value;
+let cc_cvc_input = document.querySelector('#card_cvc_input').value;
 
 
 // DOM Elements
-let cc_name_el = document.querySelector('#cardholder_name').textContent;
-const cc_number_el = document.querySelector('.card_number').textContent;
-const cc_exp_date_el = document.querySelector('.card_exp_date').textContent;
-const cc_cvc_el = document.querySelector('#card_cvc').textContent;
+
+let cc_number_el = document.querySelector('#card_number').textContent;
+let cc_exp_date_el = document.querySelector('#card_exp_date').textContent;
+let cc_cvc_el = document.querySelector('#card_cvc').textContent;
 
 
-// On Key Up CVC Number
 
-function updateElementText(){
-    cc_name_el = cc_name_input;
-
+function updateElementText(str){
+    document.querySelector('#cardholder_name').textContent = str;
 }
 
 
